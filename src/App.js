f1e6ee10c89cb7b7f6ca0d5df7d159066056d46d
RajuRk <rajurk044@gmail.com>
2022-01-10 16:31:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
+import Beef from "./components/Beef";
+import Chicken from "./components/Chicken";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import { getDefaultNormalizer } from "@testing-library/react";
+
+export const FoodContext = React.createContext();
+
+const url = "http://localhost:5000";
 
 function App() {
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  let getData = async () => {
+    let res = await axios.get(`${url}/categories`);
+    console.log(res.data);
+    setData(res.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <FoodContext.Provider value={{ url }}>
+          <Routes>
+            <Route path="/beef" element={<Beef />}></Route>
+            <Route path="/chicken" element={<Chicken />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </FoodContext.Provider>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Beef.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Beef() {
+  return <div>Beef</div>;
+}
+
+export default Beef;
